5568764fa60d9b5061ff4c1b68262be678c6f5c9
Nicholas Krämer <nicokraemer@gmx.net>
2021-02-14 12:40:07 +0100

    removed test

--- tests/test_timeseries/test_work_precision.py
+++
@@ -1,9 +0,0 @@
-"""Tests for work-precision diagram tools."""
-
-import probnumeval.timeseries as ts
-
-
-def test_work_precision():
-    wp = ts.WorkPrecision(algorithm=None)
-    out = wp.evaluate(problem=None)
-    assert isinstance(out, dict)
