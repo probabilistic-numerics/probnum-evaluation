81a3a546e6550a4d2384b0698b54ff1a178213d4
Nicholas Krämer <nicokraemer@gmx.net>
2021-02-14 11:21:53 +0100

    initialised some work precision action

--- src/probnumeval/timeseries/_work_precision.py
+++ src/probnumeval/timeseries/_work_precision.py
@@ -1,27 +1,81 @@
 """Work precision diagrams."""
 
+import time
+
+import numpy as np
+
 
 class WorkPrecision:
-    """Build-your-own work-precision diagram."""
+    """Build your own work-precision diagram.
 
-    def __init__(self, algorithm):
+    Examples
+    --------
+
+    Set up an example simulation: solve the logistic equation with scipy.integrate.solve_ivp
+
+    >>> from numpy import round, array, linspace
+    >>> from scipy.integrate import solve_ivp
+
+    >>> def algo(rtol):
+    ...     sol =  solve_ivp(lambda t, y: y*(1-y), (0., 2.), [0.1], rtol=rtol, dense_output=True)
+    ...     return sol.sol
+
+    >>> sol = algo(1e-3)
+    >>> print(sol(0.5))
+    [0.15481056]
+
+    Compute a reference solution.
+
+    >>> ref_sol = algo(1e-12)
+    >>> print(ref_sol(0.5))
+    [0.15482808]
+
+
+
+    >>> wp = WorkPrecision(algorithm=algo, ref_sol=ref_sol, evalgrid=linspace(0., 2.))
+    >>> results = wp.evaluate([1e-3, 1e-5, 1e-7, 1e-9], "rtol")
+    >>> print(results["rtol"])
+    [0.001, 1e-05, 1e-07, 1e-09]
+    >>> print(*('{:.1e}'.format(x) for x in results["rmse"]), sep=' ')
+    2.5e-05 7.2e-07 1.3e-08 1.3e-10
+    >>> print(*('{:.1e}'.format(x) for x in results["final_time"]), sep=' ')
+    4.1e-05 1.3e-06 1.4e-08 1.4e-10
+    """
+
+    def __init__(self, algorithm, ref_sol, evalgrid):
+
         self.algorithm = algorithm
+        self.ref_sol = ref_sol
+        self.evalgrid = evalgrid
 
-    def evaluate(self, problem):
+    def evaluate(self, parameters, parameter_key):
         """Run the simulations and return a dict containing the collected info."""
-        return {"problem": problem, "algorithm": self.algorithm}
 
-    def collect_walltime(self):
-        """Include wall-time in results."""
-        # pylint: disable=unnecessary-pass
-        pass
+        results = {parameter_key: [], "wall_time_sec": [], "rmse": [], "final_time": []}
 
-    def collect_rmse(self, evalgrid=None):
-        """Include RMSE in results."""
-        # pylint: disable=unnecessary-pass
-        pass
+        for par in parameters:
+            results[parameter_key].append(par)
 
-    def collect_L2(self, evalgrid=None):
-        """Include L2-error in results."""
-        # pylint: disable=unnecessary-pass
-        pass
+            start_time = time.time()
+            sol = self.algorithm(par)
+            end_time = time.time()
+            results["wall_time_sec"].append(end_time - start_time)
+
+            results["rmse"].append(compute_rmse(sol, self.ref_sol, self.evalgrid))
+            results["final_time"].append(
+                compute_final_time(sol, self.ref_sol, self.evalgrid)
+            )
+
+        return results
+
+
+def compute_rmse(sol, ref_sol, evalgrid):
+    out = sol(evalgrid)
+    ref = ref_sol(evalgrid)
+    return np.linalg.norm(out - ref) / np.sqrt(ref.size)
+
+
+def compute_final_time(sol, ref_sol, evalgrid):
+    out = sol(evalgrid[-1])
+    ref = ref_sol(evalgrid[-1])
+    return np.linalg.norm(out - ref) / np.sqrt(ref.size)
