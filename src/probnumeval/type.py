59e83c4c73724dbef8d6230594b3bffde667d225
Nicholas Krämer <nicokraemer@gmx.net>
2021-06-07 12:51:30 +0200

    ?

--- src/probnumeval/type.py
+++ src/probnumeval/type.py
@@ -8,5 +8,7 @@ from probnum import filtsmooth
 __all__ = ["ProbabilisticSolutionType", "DeterministicSolutionType"]
 
 ProbabilisticSolutionType = filtsmooth.TimeSeriesPosterior
+"""Bla1."""
 
 DeterministicSolutionType = Callable[[np.ndarray], np.ndarray]
+"""Bla1."""
